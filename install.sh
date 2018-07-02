36a4dfb7278eb02511bc40855d8fa3c63a4d38fd
Quique Piña <epina@apsl.net>
2018-07-02 09:49:13 +0200

    added xresources

--- install.sh
+++ install.sh
@@ -22,26 +22,30 @@ chsh -s $(which zsh)
 echo -e "${GREEN_COLOR}Instaling Oh-my-zsh...${NO_COLOR}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "${GREEN_COLOR}Instaling zshrc...${NO_COLOR}"
+echo -e "${GREEN_COLOR}Instaling zshrc...${NO_COLOR}"
 cp dots/zsh/zshrc ~/.zshrc
 source ~/.zshrc
 
-echo "${GREEN_COLOR}Installing wallpaper...${NO_COLOR}"
+echo -e "${GREEN_COLOR}Installing wallpaper...${NO_COLOR}"
 cp dots/wallpaper.jpg ${WALLPAPER_DIR}/.wallpaper.jpg
 
-echo "${GREEN_COLOR}Installing I3WM dotfiles...${NO_COLOR}"
+echo -e "${GREEN_COLOR}Installing I3WM dotfiles...${NO_COLOR}"
 mkdir -p ${I3_CONFIG_DIR}
 cp -r dots/i3/* ${I3_CONFIG_DIR}
 
-echo "${GREEN_COLOR}Installing Polybar dotfiles...${NO_COLOR}"
+echo -e "${GREEN_COLOR}Installing Polybar dotfiles...${NO_COLOR}"
 mkdir -p ${POLYBAR_CONFIG_DIR}
 cp -r dots/polybar/* ${POLYBAR_CONFIG_DIR}
 
-echo "${GREEN_COLOR}Installing Fonts...${NO_COLOR}"
+echo -e "${GREEN_COLOR}Installing Fonts...${NO_COLOR}"
 mkdir -p ${FONTS_DIRECTORY}
 cp -r dots/fonts/* ${FONTS_DIRECTORY}
 fc-cache -fv
 
-echo "${GREEN_COLOR}Installing NeoVim configuration...${NO_COLOR}"
+echo -e "${GREEN_COLOR}Installing NeoVim configuration...${NO_COLOR}"
 mkdir -p ${NVIM_CONFIG_DIR}
 git clone https://github.com/LordFeratum/neovimrc.git ${NVIM_CONFIG_DIR}
+
+echo -e "${GREEN_COLOR}Installing XResources...${NO_COLOR}"
+cp dots/xresources/Xresources ~/.Xresources
+xrdb -merge ~/.Xresources
